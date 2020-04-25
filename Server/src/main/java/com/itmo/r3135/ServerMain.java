5513958517090ece3eca3fb09ed315b2d51384b8
marukhlenkodaniil <daniil.marukh@gmail.com>
2020-04-25 20:33:27 +0300

    add properties support

--- Server/src/main/java/com/itmo/r3135/ServerMain.java
+++ Server/src/main/java/com/itmo/r3135/ServerMain.java
@@ -3,8 +3,9 @@ package com.itmo.r3135;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.BindException;
+import java.util.Properties;
 import java.util.Scanner;
 
 /**
@@ -12,17 +13,48 @@ import java.util.Scanner;
  * @author vladislav
  */
 public class ServerMain {
-    static final Logger logger = LogManager.getLogger("ServerStarter");
+    private static final Logger logger = LogManager.getLogger("ServerStarter");
     private final static String FILENAME = "file.json";
+    private final static String propFileName = "config.properties";
+    private final static String defPropFileName = "config.properties";
 
     public static void main(String[] args) throws IOException {
-
         logger.info("The program started.");
+        File propFile = new File(propFileName);
+
+        if (!propFile.exists()) {
+            logger.warn("Creating default config file.");
+            try (InputStream in = ServerMain.class
+                    .getClassLoader()
+                    .getResourceAsStream(defPropFileName);
+                 OutputStream out = new FileOutputStream(propFileName)) {
+                int data;
+                while ((data = in.read()) != -1) {
+                    out.write(data);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //Добавить проверку файла на пригодность
+        try (InputStream inputStream = new FileInputStream(propFileName);) {
+            logger.info("Loading setting from " + propFile.getAbsolutePath() + ".");
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            //Отладочная проверка настроек из файла
+            System.out.println(properties.getProperty("port"));
+            System.out.println(properties.getProperty("db_host"));
+            System.out.println(properties.getProperty("db_port"));
+            System.out.println(properties.getProperty("db_name"));
+            System.out.println(properties.getProperty("db_user"));
+            System.out.println(properties.getProperty("db_password"));
+        }
+
+
         Scanner input = new Scanner(System.in);
         logger.info("The port reader started.");
         while (true) {
             logger.info("To start the server, enter the port or 'exit' to exit the program.");
-//            System.out.println("Для начала работы сервера введите порт или 'exit' для завершенеия программы.");
             System.out.print("//: ");
             if (!input.hasNextLine()) {
                 break;
@@ -37,7 +69,6 @@ public class ServerMain {
                     if (port < 0 || port > 65535) {
                         logger.error("Wrong port!");
                         logger.error("Port is a number from 0 to 65535");
-//                        System.out.println("Порт - число от 0 до 65535.");
                     } else {
                         ServerWorker worker = new ServerWorker(port, FILENAME);
                         worker.startWork();
@@ -45,15 +76,11 @@ public class ServerMain {
                     }
                 } catch (NumberFormatException e) {
                     logger.error("Invalid number format in '" + inputString + "' !");
-//                    System.out.println("Ошибка в записи номера порта.");
                 } catch (BindException e) {
                     logger.error("The port " + inputString + " is busy.");
-//                    System.out.println("Этот порт уже занят.");
                 }
             }
         }
-        //   logger.info("The program has completed.");
-        //   System.out.println("Работа сервера заверщена.");
     }
 }
 
